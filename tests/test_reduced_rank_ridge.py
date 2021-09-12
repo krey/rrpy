4261d8bafb5d125b8eed19108884d294ff52201f
krey <git@lordking.pro>
2021-09-12 09:29:56 +0100

    Fixed a bad rank test & whitespace

--- tests/test_reduced_rank_ridge.py
+++ tests/test_reduced_rank_ridge.py
@@ -9,17 +9,17 @@ def test_reduced_rank_regression_intercept():
     estimator = ReducedRankRidge(alpha=10, fit_intercept=True).fit(X, Y)
     residuals = Y-estimator.predict(X)
     np.testing.assert_almost_equal(np.sum(residuals, axis=0), 0)
-    
+
 def test_reduced_rank_regresssion_no_intercept():
     X, Y = sklearn.datasets.make_regression(n_samples=100, n_features=50, n_targets=50, random_state=0, n_informative=25)
     estimator = ReducedRankRidge(fit_intercept=False).fit(X, Y)
     np.testing.assert_almost_equal(estimator.intercept_, 0)
-    
+
 def test_reduced_rank_regression_rank():
     X, Y = sklearn.datasets.make_regression(n_samples=100, n_features=50, n_targets=50, random_state=0, n_informative=25)
-    estimator = ReducedRankRidge(fit_intercept=True).fit(X, Y)
-    np.testing.assert_almost_equal(estimator.rank_, 25)
-    
+    estimator = ReducedRankRidge(fit_intercept=True, rank=20).fit(X, Y)
+    assert estimator.rank_ <= 20, f"{estimator.rank_} > 20"
+
 def test_reduced_rank_regression_vs_ridge():
     X, Y = sklearn.datasets.make_regression(n_samples=100, n_features=50, n_targets=50, random_state=0, n_informative=50)
     ridge = sklearn.linear_model.Ridge().fit(X, Y)
@@ -27,3 +27,4 @@ def test_reduced_rank_regression_vs_ridge():
     np.testing.assert_almost_equal(ridge.coef_, rrr.coef_)
     rrr = ReducedRankRidge(rank=5).fit(X, Y)
     np.testing.assert_array_less(rrr.score(X, Y), ridge.score(X, Y))
+
